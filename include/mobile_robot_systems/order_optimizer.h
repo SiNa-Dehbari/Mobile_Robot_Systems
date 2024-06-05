c3997bb7388579d05e6136ad6660d29c40f3acc3
Sina-dehbari <dehbari.sina@gmail.com>
2024-06-05 09:42:11 +0200

    Directory path argument

--- include/mobile_robot_systems/order_optimizer.h
+++ include/mobile_robot_systems/order_optimizer.h
@@ -5,6 +5,8 @@
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "mobile_robot_systems/msg/next_order.hpp"
+#include <unistd.h>
+#include <limits.h>
 #include <chrono>
 
 using namespace std::chrono_literals;
@@ -16,17 +18,18 @@ public:
 private:
 
     void DummyPublisher();
-
-    void PoseSubscriber(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
-
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
     size_t count_;
 
+    void logDirectoryPath();
+    std::string directory_path_;
+
+    void PoseSubscriber(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscription_;
 
-    rclcpp::Subscription<mobile_robot_systems::msg::NextOrder>::SharedPtr subscription_;
     void NextOrderSubscriber(const mobile_robot_systems::msg::NextOrder::SharedPtr msg);
+    rclcpp::Subscription<mobile_robot_systems::msg::NextOrder>::SharedPtr subscription_;
 };
 
 #endif  // ORDER_OPTIMIZER_PUBLISHER__ORDER_OPTIMIZER_PUBLISHER_H_
--- src/order_optimizer.cpp
+++ src/order_optimizer.cpp
@@ -2,10 +2,26 @@
 
 OrderOptimizer::OrderOptimizer() : Node("OrderOptimizer"), count_(0)
 {
+    // Dummy Publisher_ to be cleaned
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
     timer_ = this->create_wall_timer(
         500ms, std::bind(&OrderOptimizer::DummyPublisher, this));
 
+    // File path arguments
+    this->declare_parameter<std::string>("directory_path", "");
+
+    std::string param_directory_path;
+    if (this->get_parameter("directory_path", param_directory_path) && !param_directory_path.empty()) {
+        directory_path_ = param_directory_path;
+    } else {
+        // Get the current working directory
+        char cwd[PATH_MAX];
+        getcwd(cwd, sizeof(cwd));
+        directory_path_ = std::string(cwd) + "/src/mobile_robot_systems/files";
+        RCLCPP_WARN(this->get_logger(), "Directory path parameter not set, using default: %s", directory_path_.c_str());
+    }
+    logDirectoryPath();
+    //
     pose_subscription_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
     "currentPosition", 10, std::bind(&OrderOptimizer::PoseSubscriber, this, std::placeholders::_1));
 
@@ -18,9 +34,13 @@ void OrderOptimizer::DummyPublisher()
 {
     auto message = std_msgs::msg::String();
     message.data = "Hello, world! " + std::to_string(count_++);
-    //RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
 };
+
+void OrderOptimizer::logDirectoryPath() {
+    RCLCPP_INFO(this->get_logger(), "Using directory path: %s", directory_path_.c_str());
+}
 
 void OrderOptimizer::PoseSubscriber(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
   {
